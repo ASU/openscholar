269894cd9c8c510c0d762dbac8bd8e966d193d52
Jared Rounsavall <jaredrounsavall@MacBook-Pro-2.local>
2017-03-27 09:12:11 -0700

    Adding vsite_restful_export placeholder generated by mike

---
+++ openscholar/modules/vsite/modules/vsite_export_restful/plugins/restful/node/faq/1.0/FAQNodeRestfulBase.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class FAQNodeRestfulBase extends VsiteExportNodeRestfulBase {
+
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    return $public_fields;
+  }
+
+}
