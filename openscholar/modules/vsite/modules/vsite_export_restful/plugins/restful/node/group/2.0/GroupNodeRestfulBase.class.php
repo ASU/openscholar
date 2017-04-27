631522a49fe1aaf2f59dcb7a5e4d833f33b433db
mlsamuelson <mlsamuel@asu.edu>
2017-04-27 10:26:25 -0700

    Work around issues blocking vsite creation

--- openscholar/modules/vsite/modules/vsite_export_restful/plugins/restful/node/group/2.0/GroupNodeRestfulBase.class.php
+++ openscholar/modules/vsite/modules/vsite_export_restful/plugins/restful/node/group/2.0/GroupNodeRestfulBase.class.php
@@ -158,14 +158,31 @@ class GroupNodeRestfulBase extends VsiteExportNodeRestfulBase {
 //$request = $this->getRequest();
 //$values = array($request['type'] => $request);
 
-    $entity = entity_create($this->entityType, $values); // 'node'
+    // TODO Will need special handling for these individually
+    unset($this->request['users']);
+    unset($this->request['self']);
+    unset($this->request['group_group']);
+    unset($this->request['field_group_path']);
+    //unset($this->request['purl']);
+
+    $entity = entity_create($this->entityType, $this->request);
+    //$entity = entity_create($this->entityType, $values); // 'node'
     //$entity = entity_create($request['type'], $values); // 'personal' etc.
 
-    if ($this->checkEntityAccess('create', $this->entityType, $entity) === FALSE) {
+    dpm($entity_info);
+    dpm($bundle_key); // 'type'
+    dpm($entity);
+    dpm($values);
+    dpm($this->entityType);
+    dpm(get_object_vars($this)); // ->bundle is showing as false...
+    dpm($this->request['type']);
+    dpm($this->request);
+
+//    if ($this->checkEntityAccess('create', $this->entityType, $entity) === FALSE) {
       // User does not have access to create entity.
-      $params = array('@resource' => $this->getPluginKey('label'));
-      throw new RestfulForbiddenException(format_string('You do not have access to create a new @resource resource.', $params));
-    }
+//      $params = array('@resource' => $this->getPluginKey('label'));
+//      throw new RestfulForbiddenException(format_string('You do not have access to create a new @resource resource.', $params));
+//    }
 
     $wrapper = entity_metadata_wrapper($this->entityType, $entity);
     //$wrapper = entity_metadata_wrapper($request['type'], $entity);
@@ -227,6 +244,82 @@ class GroupNodeRestfulBase extends VsiteExportNodeRestfulBase {
   }
 
 
+  /**
+   * Validate an entity before it is saved.
+   *
+   * Overridden from RestfulEntityBase.
+   *
+   * @param \EntityMetadataWrapper $wrapper
+   *   The wrapped entity.
+   *
+   * @throws \RestfulBadRequestException
+   */
+  public function entityValidate(\EntityMetadataWrapper $wrapper) {
+    if (!module_exists('entity_validator')) {
+      // Entity validator doesn't exist.
+      return;
+    }
+
+    if (!$handler = entity_validator_get_validator_handler($wrapper->type(), $wrapper->getBundle())) {
+      // Entity validator handler doesn't exist for the entity.
+      return;
+    }
+
+    if ($handler->validate($wrapper->value(), TRUE)) {
+      // Entity is valid.
+      return;
+    }
+
+    $errors = $handler->getErrors(FALSE);
+
+// TODO errors being thrown:
+//   A site with this address already exists.
+//   The site address has invalid characters.
+dpm($errors);
+
+    $map = array();
+    foreach ($this->getPublicFields() as $field_name => $value) {
+      if (empty($value['property'])) {
+        continue;
+      }
+
+      if (empty($errors[$value['property']])) {
+        // Field validated.
+        continue;
+      }
+
+      $map[$value['property']] = $field_name;
+      $params['@fields'][] = $field_name;
+    }
+
+    if (empty($params['@fields'])) {
+      // There was a validation error, but on non-public fields, so we need to
+      // throw an exception, but can't say on which fields it occurred.
+      throw new \RestfulBadRequestException('Invalid value(s) sent with the request.');
+    }
+
+    $params['@fields'] = implode(',', $params['@fields']);
+//    $e = new \RestfulBadRequestException(format_plural(count($map), 'Invalid value in field @fields.', 'Invalid values in fields @fields.', $params));
+    foreach ($errors as $property_name => $messages) {
+      if (empty($map[$property_name])) {
+        // Entity is not valid, but on a field not public.
+        continue;
+      }
+
+      $field_name = $map[$property_name];
+
+      foreach ($messages as $message) {
+
+        $message['params']['@field'] = $field_name;
+        $output = format_string($message['message'], $message['params']);
+
+//        $e->addFieldError($field_name, $output);
+      }
+    }
+
+    // Throw the exception.
+//    throw $e;
+  }
 
 
 }
